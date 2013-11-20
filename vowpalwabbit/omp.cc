c4f560ea0f484edd4923209702791aacaeaa9aea
Alex Grubb <agrubb@cmu.edu>
2013-11-20 01:31:51 -0500

    skeleton omp functions

--- vowpalwabbit/omp.cc
+++ vowpalwabbit/omp.cc
@@ -11,24 +11,78 @@ using namespace std;
 namespace OMP {
 
   struct omp {
+    bool gradient_pass;
+    bool initialization_needed;
+
     vw* all;
   };
+
+  void start_gradient_pass(omp* o)
+  {
+    vw* all = o->all;
+
+    if (!all->quiet) {
+      cerr << " ** starting gradient pass." << endl;
+    }
+
+    o->gradient_pass = true;
+  }
+
+  void finish_gradient_pass(omp* o)
+  {
+    vw* all = o->all;
+
+    if (!all->quiet) {
+      cerr << " ** finishing gradient pass." << endl;
+    }
+
+    o->gradient_pass = false;
+  }
+
+  void accumulate_gradient(omp* o, example* ec)
+  {
+    vw* all = o->all;
+  }
 
   void learn(void* d, learner& base, example* ec)
   {
     omp* o = (omp*)d;
     vw* all = o->all;
 
-    base.learn(ec);
+    if (o->initialization_needed) {
+      start_gradient_pass(o);
+      o->initialization_needed = false;
+    }
+
+    if (o->gradient_pass) {
+      accumulate_gradient(o, ec);
+    }
+    else {
+      base.learn(ec);
+    }
+  }
+
+  void end_pass(void* d)
+  {
+    omp* o = (omp*)d;
+    vw* all = o->all;
+
+    if (o->gradient_pass) {
+      finish_gradient_pass(o);
+    }
   }
 
   learner* setup(vw& all, std::vector<std::string>&opts, po::variables_map& vm, po::variables_map& vm_file)
   {
     omp* data = (omp*)calloc(1, sizeof(omp));
 
+    data->initialization_needed = true;
+    data->gradient_pass = true;
+
     data->all = &all;
 
     learner* l = new learner(data, learn, all.l);
+    l->set_end_pass(end_pass);
 
     return l;
   }
